277b71bdb715aefa9363a093cbd7b3cae6a65438
Sakib <ssar96540@gmail.com>
2024-03-14 15:30:22 +0600

    Added tailwind , daisiUi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className='p-4'>
+    <h1 className='text-4xl text-center'>Course Registration</h1>
+   </div>
+  )
+}
+
+export default App
